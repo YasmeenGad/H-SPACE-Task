674752177c57c5e788a87d56af6759aa0d73ccd2
YasmeenGad <gadyasmeen335@gmail.com>
2025-04-06 20:25:56 +0300

    chore: create app router file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'core/routes/app_router.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-
+      routerConfig: AppRouter.router,
     );
   }
 }
